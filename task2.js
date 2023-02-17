a3f5c65bba094121444621ef2a9191b38031f2fb
sasha <sasharuban@MacBook-Air-Sasha.local>
2023-02-17 04:10:58 +0300

    style changes

--- task2.js
+++ task2.js
@@ -3,7 +3,7 @@ function getNumberRadix(number, radix) {
   let rad = +radix;
 
   if (num !== num || num <= 0 || rad !== rad || rad < 2 || rad > 16) {
-    throw Error("Функция getNumberRadix была вызвана с некорректными параметрами");
+    throw Error('Функция getNumberRadix была вызвана с некорректными параметрами');
   }
   return num.toString(rad);
 }
